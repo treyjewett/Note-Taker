f29ab63c80acc666d17d577ec2a14908d1fdd4c1
Russell Jewett <rfjewett3@gmail.com>
2021-01-19 17:51:36 -0500

    fixes index.js file paths on both html docs. App is working properly.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('./public'))
 
 // Setting up routes for when the user inputs their notes.
 require("./routes/apiRoute")(app);
