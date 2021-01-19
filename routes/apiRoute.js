5a7ae28481f206c3bf28709ca0ee232e01747e99
Russell Jewett <rfjewett3@gmail.com>
2021-01-18 22:21:06 -0500

    Updates HTML route. API route still needs work.

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -2,5 +2,13 @@ let noteData = require('../db/db.json');
 const fs = require('fs');
 
 module.exports = function(app) {
-    
+    app.get('/api', function(req, res) {
+        res.json(index);
+    });
+
+    app.get('/api/notes', function(req, res) {
+        res.json(db.json);
+    });
+
+    app.post
 }
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Set up the dependencies.
 const express = require('express');
 
 const app = express();
@@ -7,9 +8,11 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Setting up routes for when the user inputs their notes.
 require("./routes/apiRoute")(app);
 require("./routes/htmlRoute")(app);
 
+// Runs the server.
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });
